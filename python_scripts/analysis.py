86b70470dd0226e8cc3518756b67356731cc1ac3
Melle Bolding <136085353+mellebolding@users.noreply.github.com>
2025-08-14 14:32:47 +0200

    a

--- python_scripts/analysis.py
+++ python_scripts/analysis.py
@@ -509,12 +509,12 @@ I_norm1_sub = np.squeeze(np.array(get_field(all_values, "I_norm1", filters={"lev
 I_norm2_sub = np.squeeze(np.array(get_field(all_values, "I_norm2", filters={"level": "subject"})), axis=0)
 
 
-figures_I_tmax_norm1_norm2(group=True, subject=False, I_tmax=I_tmax_group, I_norm1=I_norm1_group, I_norm2=I_norm2_group)
-figures_I_tmax_norm1_norm2(group=False, subject=True, I_tmax=I_tmax_sub, I_norm1=I_norm1_sub, I_norm2=I_norm2_sub)
+# figures_I_tmax_norm1_norm2(group=True, subject=False, I_tmax=I_tmax_group, I_norm1=I_norm1_group, I_norm2=I_norm2_group)
+# figures_I_tmax_norm1_norm2(group=False, subject=True, I_tmax=I_tmax_sub, I_norm1=I_norm1_sub, I_norm2=I_norm2_sub)
 
-figures_barplot_parcels('I_tmax',I_tmax_group, NPARCELLS)
-figures_barplot_parcels('I_norm1', I_norm1_group, NPARCELLS)
-figures_barplot_parcels('I_norm2', I_norm2_group, NPARCELLS)
+# figures_barplot_parcels('I_tmax',I_tmax_group, NPARCELLS)
+# figures_barplot_parcels('I_norm1', I_norm1_group, NPARCELLS)
+# figures_barplot_parcels('I_norm2', I_norm2_group, NPARCELLS)
 
 
 ##### RESTING STATE NETWORKS #####
@@ -536,12 +536,12 @@ RSNs = {
     'SalVentAttn': SalVentAttn
 }
 
-# plot_means_per_RSN('I_tmax', I_tmax_group, NPARCELLS)
-# plot_means_per_RSN('I_norm1', I_norm1_group, NPARCELLS)
-# plot_means_per_RSN('I_norm2', I_norm2_group, NPARCELLS)
+plot_means_per_RSN('I_tmax', I_tmax_group, NPARCELLS)
+plot_means_per_RSN('I_norm1', I_norm1_group, NPARCELLS)
+plot_means_per_RSN('I_norm2', I_norm2_group, NPARCELLS)
 
-# plot_means_per_subjects_per_RSN(SomMot, I_tmax_sub, 'SomMot', 'I_tmax', NPARCELLS)
-# plot_means_per_subjects_per_RSN(Vis, I_tmax_sub, 'Vis', 'I_tmax', NPARCELLS)
+plot_means_per_subjects_per_RSN(SomMot, I_tmax_sub, 'SomMot', 'I_tmax', NPARCELLS)
+plot_means_per_subjects_per_RSN(Vis, I_tmax_sub, 'Vis', 'I_tmax', NPARCELLS)
 # #...
 
 ###### VISUALIZATION ######
