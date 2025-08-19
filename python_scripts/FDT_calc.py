374359c02fa3bb82b2ed461df8c9a58c394c5785
Melle Bolding <136085353+mellebolding@users.noreply.github.com>
2025-08-19 12:02:55 +0200

    a

--- python_scripts/FDT_calc.py
+++ python_scripts/FDT_calc.py
@@ -287,7 +287,7 @@ omega_subs = [HC_subs_omega, MCI_subs_omega, AD_subs_omega]
 a_group = np.vstack(get_field(all_records, "a", filters={"level": "group"}))
 a_subs = np.vstack(get_field(all_records, "a", filters={"level": "subject"}))
 a_group_org = np.array(get_field(all_records, "original_a", filters={"level": "group"}))
-a_subs_org = np.array(get_field(all_records, "original_a", filters={"level": "subject"}))
+a_subs_org = get_field(all_records, "original_a", filters={"level": "subject"})
 
 # group analysis
 I_tmax_group,I_norm1_group,I_norm2_group = FDT_group_Itmax_norm1_norm2(sigma_group, Ceff_group, omega, a_group, gconst=1.0, v0bias=0.0, tfinal=200, dt=0.01, tmax=100, ts0=0)
