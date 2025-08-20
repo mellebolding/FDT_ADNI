fa49e6c3f54d05fdc84545563f7d921667366c8b
Melle Bolding <136085353+mellebolding@users.noreply.github.com>
2025-08-20 11:19:26 +0200

    a

--- python_scripts/analysis.py
+++ python_scripts/analysis.py
@@ -595,7 +595,7 @@ I_tmax_sub = np.squeeze(np.array(get_field(all_values, "I_tmax", filters={"level
 I_norm1_sub = np.squeeze(np.array(get_field(all_values, "I_norm1", filters={"level": "subject"})), axis=0)
 I_norm2_sub = np.squeeze(np.array(get_field(all_values, "I_norm2", filters={"level": "subject"})), axis=0)
 
-print("a_values_group shape: ", a_values_group.shape)
+print("a_values_group shape: ", a_values_group)
 
 # figures_I_tmax_norm1_norm2(group=True, subject=False, I_tmax=I_tmax_group, I_norm1=I_norm1_group, I_norm2=I_norm2_group)
 # if A_FITTING: figures_I_tmax_norm1_norm2(group=True, subject=False, I_tmax=I_tmax_group_a, I_norm1=I_norm1_group_a, I_norm2=I_norm2_group_a,a=A_FITTING)
