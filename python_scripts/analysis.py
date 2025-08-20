5043cf331b744c4aecf7045528652dad2ed8e7cd
Melle Bolding <136085353+mellebolding@users.noreply.github.com>
2025-08-20 10:16:32 +0200

    a

--- python_scripts/analysis.py
+++ python_scripts/analysis.py
@@ -607,7 +607,7 @@ I_norm2_sub = np.squeeze(np.array(get_field(all_values, "I_norm2", filters={"lev
 # figures_barplot_parcels('I_norm2', I_norm2_group, NPARCELLS)
 # if A_FITTING: figures_barplot_parcels('I_norm2', I_norm2_group_a, NPARCELLS, a=A_FITTING)
 
-
+print(a_original_group.shape, a_original_sub.shape)
 ##### RESTING STATE NETWORKS #####
 SomMot = [7, 8, 23, 35, 38, 39, 40, 42, 50, 52, 54, 55, 56, 98, 99, 100, 101, 102, 103, 104, 105, 106, 114, 123, 124, 167, 172, 173, 174, 187, 188, 191, 203, 207, 215, 218, 219, 220, 221, 230, 232, 233, 234, 235, 279, 280, 281, 282, 283, 284, 303, 347, 352, 353, 354]
 Vis = [0, 1, 2, 3, 4, 5, 6, 12, 15, 17, 18, 19, 20, 21, 22, 118, 119, 120, 125, 126, 141, 142, 145, 151, 152, 153, 154, 155, 156, 157, 158, 159, 162, 180, 181, 182, 183, 184, 185, 186, 192, 195, 198, 199, 200, 201, 202, 300, 322, 331, 332, 333, 335, 337, 338, 339, 342]
