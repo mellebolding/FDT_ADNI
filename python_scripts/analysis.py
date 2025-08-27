1738e6573f3025549e3fdaa548e87bcf5dcfa6ce
Melle Bolding <melleb11@gmail.com>
2025-08-27 12:10:18 +0200

    a

--- python_scripts/analysis.py
+++ python_scripts/analysis.py
@@ -740,7 +740,7 @@ if A_FITTING:
     diff_org_a_group = np.subtract(a_values_group, a_002)
 #print("diff a sub: ", diff_a_sub)
 
-I_norm2_select = np.array([I_norm2_sub[0,0,:], I_norm2_sub[1,0,:], I_norm2_sub[2,0,:]])
+I_norm2_select = np.array([I_norm2_sub[0,1,:], I_norm2_sub[1,1,:], I_norm2_sub[2,0,:]])
 X_norm2_select = np.array([X_norm2_sub[0,0,:], X_norm2_sub[1,0,:], X_norm2_sub[2,0,:]])
 
 I_vs_Xnorm2(I_norm2_group, X_norm2_group, a=False)
