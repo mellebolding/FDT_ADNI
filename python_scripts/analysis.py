25c6eb67c2e712984248943f08021e7e82c847fd
Melle Bolding <melleb11@gmail.com>
2025-08-28 16:09:43 +0200

    a

--- python_scripts/analysis.py
+++ python_scripts/analysis.py
@@ -997,6 +997,7 @@ if A_FITTING:
     plt.tight_layout()
     #plt.show()
 
+print(ABeta_burden[1].shape, Tau_burden[1].shape, I_norm2_sub[1].shape)
 df_list = []
 for cohort_idx, (AB, Tau, FDTI) in enumerate(zip(ABeta_burden, Tau_burden, I_norm2_sub)):
     nsub, nparcel = AB.shape
