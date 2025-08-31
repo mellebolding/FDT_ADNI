5c240a2d8a3c26fe230a7d1ee14c2e144ba7385b
Melle Bolding <melleb11@gmail.com>
2025-08-31 18:31:41 +0200

    a

--- python_scripts/analysis.py
+++ python_scripts/analysis.py
@@ -212,7 +212,7 @@ def I_vs_Xnorm2(I_norm2_group, X_norm2_group, I_a=None,X_a=None,a=False, sub=Fal
         I_norm2_group = np.concatenate((I_norm2_group, I_a), axis=0)
         X_norm2_group = np.concatenate((X_norm2_group, X_a), axis=0)
         group_names = ['HC', 'MCI', 'AD', 'HC_a', 'MCI_a', 'AD_a']
-        colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:cyan', 'tab:red', 'tab:olive']
+        colors = ['tab:blue', 'tab:orange', 'tab:green', 'navy', 'darkorange', 'darkgreen']
 
     if sub: 
         I_norm2_group = np.nanmean(I_norm2_group, axis=2)
