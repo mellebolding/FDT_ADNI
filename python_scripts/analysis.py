92f6448f9fb13d435edb83623650692be5a4e4dd
Melle Bolding <melleb11@gmail.com>
2025-09-02 12:39:33 +0200

    a

--- python_scripts/analysis.py
+++ python_scripts/analysis.py
@@ -717,11 +717,9 @@ def load_PET_data(NPARCELLS):
     AD_ABeta = []
     AD_Tau = []
     for subject in HC_IDs:
-        print(subject)
         data = DL.get_subjectData(subject,printInfo=False)
         HC_ABeta.append(np.vstack(data[subject]['ABeta'])) 
         HC_Tau.append(np.vstack(data[subject]['Tau']))
-    print(HC_ABeta[0][:,0])
     for subject in MCI_IDs:
         data = DL.get_subjectData(subject,printInfo=False)
         MCI_ABeta.append(np.vstack(data[subject]['ABeta']))
@@ -854,8 +852,8 @@ I_norm2_select = np.array([I_norm2_sub[0,0,:], I_norm2_sub[1,0,:], I_norm2_sub[2
 X_norm2_select = np.array([X_norm2_sub[0,0,:], X_norm2_sub[1,0,:], X_norm2_sub[2,0,:]])
 
 fig, ax = plt.subplots(figsize=(6, 5))
-plot_boxes_HC_MCI_AD(ax=ax,data=ABeta_burden,saveplot=1,metric='ABeta burden',title='Abeta burden subject-average across groups',filename=f'Abeta_burden_N{NPARCELLS}_{NOISE_TYPE}_a{A_FITTING}',folder=FDT_subject_subfolder)
-plot_boxes_HC_MCI_AD(ax=ax,data=Tau_burden,saveplot=1,metric='Tau burden',title='Tau burden subject-average across groups',filename=f'Tau_burden_N{NPARCELLS}_{NOISE_TYPE}_a{A_FITTING}',folder=FDT_subject_subfolder)
+plot_boxes_HC_MCI_AD(ax=ax,data=ABeta_burden,saveplot=1,metric='ABeta burden',plot_title='Abeta burden subject-average across groups',filename=f'Abeta_burden_N{NPARCELLS}_{NOISE_TYPE}_a{A_FITTING}',folder=FDT_subject_subfolder)
+plot_boxes_HC_MCI_AD(ax=ax,data=Tau_burden,saveplot=1,metric='Tau burden',plot_title='Tau burden subject-average across groups',filename=f'Tau_burden_N{NPARCELLS}_{NOISE_TYPE}_a{A_FITTING}',folder=FDT_subject_subfolder)
 # I_vs_Xnorm2(I_norm2_group_a, X_norm2_group_a, a=True)
 # I_vs_Xnorm2(I_norm2_sub_a, X_norm2_sub_a, a=True, sub=True)
 # if A_FITTING: I_vs_Xnorm2(I_norm2_group, X_norm2_group,I_norm2_group_a,X_norm2_group_a, a=True)
