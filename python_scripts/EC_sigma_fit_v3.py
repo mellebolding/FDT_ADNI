cc93ba19f00a419603cade52981d1dfa4896cf02
Melle Bolding <melleb11@gmail.com>
2025-09-18 09:43:21 +0200

    a

--- python_scripts/EC_sigma_fit_v3.py
+++ python_scripts/EC_sigma_fit_v3.py
@@ -375,7 +375,7 @@ def show_error(error_iter, errorFC_iter, errorCOVtau_iter, sigma, sigma_ini, a, 
 
 ##### Base parameters settings ######
 
-NPARCELLS = 379 # max 379
+NPARCELLS = 45 # max 379
 CEFF_FITTING = True
 SIGMA_FITTING = True
 A_FITTING = True
